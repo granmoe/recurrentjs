87f4043fc08bde1b32ce094f1a5d594d2ab25c23
Matt Granmoe <mattgranmoe@gmail.com>
2018-05-18 22:42:19 -0500

    Throw RNN output into UI, along with other info that is kept on component state

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,9 @@ export default class App extends Component {
     intervalId: null,
     hasRun: false,
     temperature: 1,
+    samples: [],
+    argMaxPrediction: null,
+    iterations: 0,
   }
 
   rnnModel = null
@@ -29,9 +32,7 @@ export default class App extends Component {
       temperature: this.state.temperature,
     })
     if (this.iter % 10 === 0) {
-      console.log(`argMaxPrediction: ${argMaxPrediction}`)
-      console.log(`sample: ${samples[0]}`)
-      console.log(`iterations: ${iterations}`)
+      this.setState({ argMaxPrediction, samples, iterations })
     }
   }
 
@@ -74,6 +75,7 @@ export default class App extends Component {
           <Text>Here is some info about RNNs</Text>
           <Divider w={1} color="blue" />
           <Heading>Experiment with RNN models in the browser</Heading>
+          <br />
           <Button onClick={this.pauseOrResume}>
             {!this.state.hasRun
               ? 'Start'
@@ -81,15 +83,41 @@ export default class App extends Component {
                 ? 'Pause'
                 : 'Resume'}
           </Button>
+          {/* TODO: Refactor away all these <br>s */}
+          <br />
+          <br />
           <Slider
             value={this.state.temperature}
             min="0.1"
             max="9"
             step="0.1"
             onChange={e => {
-              this.setState({ temperature: e.target.value })
+              this.setState({ temperature: Number(e.target.value) })
             }}
           />
+          <br />
+          <Text>
+            <br />
+            <div>
+              Samples:
+              <br />
+              <br />
+              {this.state.samples.map((sample, i) => (
+                <div key={i}>{sample}</div>
+              ))}
+            </div>
+            <br />
+            <div>
+              Argmax Prediction:
+              <br />
+              <br />
+              {this.state.argMaxPrediction}
+            </div>
+            <br />
+            <div>Iterations: {this.state.iterations}</div>
+            <br />
+            <div>Sample temperature: {this.state.temperature}</div>
+          </Text>
         </Container>
       </Provider>
     )
